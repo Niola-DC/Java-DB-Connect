92abb9a0996b2900fe0430671f5c277d3a9f63c6
Eniola Fanegan <eniolachinemerem74@gmail.com>
2023-11-20 10:32:18 +0100

    Add files via upload
    This code has an exception

---
+++ DataCheck/src/main/java/com/mycompany/datacheck/DisplayTable.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.datacheck;
+
+/**
+ *
+ * @author Donna
+ */
+import java.sql.*;
+
+public class DisplayTable {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306//zero";
+        String name = "root";
+        String password = "Java2023!";
+        String query = "SELECT * FROM persons"; // Replace with your table name
+
+        try {
+            Connection connection = DriverManager.getConnection(url, name, password);
+                System.out.println("This is working");
+                
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(query);
+                
+                ResultSetMetaData metaData = resultSet.getMetaData();
+                int columnCount = metaData.getColumnCount();
+                
+                // Print table header- column name
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.printf("%-20s", metaData.getColumnName(i));
+                }
+                System.out.println();
+                System.out.println("This is working");
+                
+                
+                // Print table rows
+                while (resultSet.next()) {
+                    for (int i = 1; i <= columnCount; i++) {
+                        System.out.printf("%-20s", resultSet.getString(i));
+                    }
+                    System.out.println();
+                    System.out.println("This is working");
+                    
+                }
+                
+                statement.close();
+                resultSet.close();
+        }
+         catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
